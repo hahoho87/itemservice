b0c3226f711f0287d4662875325b853b6fb7bbba
bky4564 <bky4564@hunet.co.kr>
2021-08-03 09:04:47 +0900

    Initial commit

---
+++ src/test/java/com/hahoho87/hello/itemservice/ItemserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hahoho87.hello.itemservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItemserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
